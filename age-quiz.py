3cc40ff4c7a75d64a2c177323741805128d2f9bc
gitgit-hooray <thecharmainedomain@gmail.com>
2024-03-05 20:44:38 +0000

    Comment in line 18 & 19 the change to filename Move line 6, ‘age = int(input……’  with the corresponding comment , to line 28 & 29

--- age-quiz.py
+++ age-quiz.py
@@ -1,21 +1,21 @@
 """ This is an if-elif-else statement that prints out different 
 messages according to the user's age. """
 
-# int() function casts users age input, from string to an integer.
-
-age = int(input("Please enter your age: "))
-
 # Arrange if-elif statements in descending order regarding age.
-# If the condition, 'age >= 40', was put as the starting if statement, 
-# the flow of logic would overide the next elif statements 
+# If the condition, 'age >= 40', was put as the starting if statement,
+# the flow of logic would overide the next elif statements
 # >= 65 and > 100 and their respective messages would not be output.
 
 # This is because the conditional, 'age >=40', covers all numbers over 40,
-# so the flow of logic would output the first conditional 
+# so the flow of logic would output the first conditional
 # and not need to output the next two.
 
 # Open the file in read mode
-with open('/Users/charmainefernandes/Documents/Documents/Cogrammar/CF23100010231/Data Science (Fundamentals)/T23 - Version Control and Git/Projects/Easiest Age calculator/age-quiz/driver-info.txt', 'r') as file:
+# 'drivers.txt' filename was changed to 'drivers-info.txt' manually,
+# pathname had been altered to show the filename change
+with open('/Users/charmainefernandes/Documents/Documents/Cogrammar/\
+CF23100010231/Data Science (Fundamentals)/T23 - Version Control and \
+Git/Projects/Easiest Age calculator/age-quiz/driver-info.txt', 'r') as file:
     # Read the drivers from the file
     drivers = file.readlines()
 
@@ -24,6 +24,9 @@ for driver_info in drivers:
     # Process driver_info as needed
     print(driver_info.strip())  # For example, printing each line
 
+
+# int() function casts users age input, from string to an integer.
+age = int(input("Please enter your age: "))
 
 if age > 100:
     print("Sorry, you're dead.")
@@ -56,3 +59,4 @@ elif age == 21:
     print("Congrats on your 21st!")
 else:
     print("Age is but a number.")
+    
